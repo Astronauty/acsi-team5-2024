ca64cac5b1f9544e65f8a3cd9de93a053f06b2ee
Daniel Nguyen <=>
2024-11-07 21:01:32 -0500

    added osqp hover

---
+++ osqp_hover.py
@@ -0,0 +1,14 @@
+import logging
+import sys
+import time
+import osqp
+from threading import Event
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.utils import uri_helper
+
+URI = uri_helper.uri_from_env(default='radio://0/20/2M/E7E7E7E701')
